17e75fdf1f2553584db0e38ee42dd8f8c85d16a8
parbat10 <parbatlimbu139@gmail.com>
2025-08-13 15:25:24 +0545

    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+    function handleCostKeydown(event){
+        if(event.key === 'Enter'){
+            calculateTotal()
+        }
+    }
+    function calculateTotal(){
+        const inputElement = document.querySelector('.js-cost-input');
+        let cost = Number(inputElement.value) ;
+
+            if ( cost < 40){
+                cost = cost +10;
+            }
+
+            document.querySelector('.js-total-cost')
+             .innerHTML = `$${cost}`;
+    }
+    function Subscribe() {
+            const buttonElement = document.querySelector('.js-Subscribe-button');
+
+            if( buttonElement.innerText === 'Subscribe'){
+                buttonElement.innerHTML = 'Subscribed';
+            }else{
+                buttonElement.innerHTML = 'Subscribe';
+            }
+    }
