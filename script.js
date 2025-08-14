9ca095885f37e02f0c51b15faaa084762de3b586
parbat10 <parbatlimbu139@gmail.com>
2025-08-14 16:09:26 +0545

    css updating

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 
             if( buttonElement.innerText === 'Subscribe'){
                 buttonElement.innerHTML = 'Subscribed';
+                buttonElement.classList.add('is-subscribed');
             }else{
                 buttonElement.innerHTML = 'Subscribe';
+                buttonElement.classList.remove('is-subscribed')
             }
     }
     function handleCostKeydown(event){
